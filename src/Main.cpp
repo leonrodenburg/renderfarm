0d7f18afbfa6a272439299ac833a92c0146f8005
Leon Rodenburg <l.rodenburg@instanceof.nl>
2012-09-24 12:17:52 +0200

    Replaced tabs with spaces

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,24 +5,24 @@
 
 int main(int argc, char** argv)
 {
-	std::cout << "Renderfarm 0.1a - by Leon Rodenburg" << std::endl;
+    std::cout << "Renderfarm 0.1a - by Leon Rodenburg" << std::endl;
 
-	RFMath::Matrix matTrans;
-	matTrans.Translate(10.0f, 0.0f, 0.0f);
+    RFMath::Matrix matTrans;
+    matTrans.Translate(10.0f, 0.0f, 0.0f);
 
-	RFMath::Matrix matScale;
-	matScale.Scale(10.0f, 1.0f, 1.0f);
+    RFMath::Matrix matScale;
+    matScale.Scale(10.0f, 1.0f, 1.0f);
 
-	RFMath::Matrix matRot;
-	matRot.RotationY(RFMathDegToRad(90));
+    RFMath::Matrix matRot;
+    matRot.RotationY(RFMathDegToRad(90));
 
-	RFMath::Matrix mat = matScale * matTrans * matRot;
+    RFMath::Matrix mat = matScale * matTrans * matRot;
 
-	RFMath::Vector3 vec(10.0f, 5.0f, 0.0f);
-	RFMath::Vector3 result = vec * mat;
+    RFMath::Vector3 vec(10.0f, 5.0f, 0.0f);
+    RFMath::Vector3 result = vec * mat;
 
-	std::cout << result << std::endl;
+    std::cout << result << std::endl;
 
-	std::getchar();
-	return 0;
+    std::getchar();
+    return 0;
 }
